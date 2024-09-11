b68dc456e98211e11ae2ce2e046512439194184d
Hèctor Damiàn Simañuk <dsimanuk@suppliers.tenaris.com>
2024-09-11 09:52:58 -0300

    demo blazor orignal

---
+++ Source/DemoOrigianl/DemoOrigianl/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+namespace DemoOrigianl.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
